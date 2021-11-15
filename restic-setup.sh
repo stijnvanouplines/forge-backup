9f297309e51aa423557b78e4bcbd70eda0e7dddb
Stijn Vanouplines <stijn@solitweb.be>
2021-11-15 14:11:49 +0100

    Use AWS S3 instead of Backblaze B2

--- restic-setup.sh
+++ restic-setup.sh
@@ -2,18 +2,18 @@
 
 ###############################################
 #
-# Forge → B2 Backup
-# Backs up all databases and /home/forge to
-# Backblaze B2 using restic.
+# Forge Backup
+# Backs up all sites and databases
+# to AWS S3 using restic.
 #
-# by Matt Stein <matt@workingconcept.com>
+# Inspired by https://github.com/workingconcept
 # No warranty provided, use at your own risk!
 #
 ###############################################
 # Instructions
 ###############################################
 #
-# 1. Create a B2 bucket and application key.
+# 1. Create a S3 bucket and application key.
 # 2. Run this script as root. (Not sudo.)
 # 3. Run a mysql backup with mysql-backup.sh.
 # 4. Run a backup with restic-backup.sh.
@@ -25,21 +25,24 @@
 
 echo ""
 echo "----------------------------------------"
-echo "Forge → B2 Backup"
+echo "Forge Backup"
 echo "----------------------------------------"
 echo ""
 
-if [ ! -f /root/restic/conf/b2.conf ]; then
-    read -s -p "Enter B2 application key ID: " B2_ACCOUNT_ID
+if [ ! -f /root/restic/conf/s3.conf ]; then
+    read -s -p "Enter AWS Access Key ID: " AWS_ACCESS_KEY_ID
     echo ""
-    read -s -p "Enter B2 application key: " B2_ACCOUNT_KEY
+    read -s -p "Enter AWS Secret Access Key: " AWS_SECRET_ACCESS_KEY
     echo ""
-    read -s -p "Enter B2 bucket: " B2_BUCKET
+    read -s -p "Enter S3 bucket: " S3_BUCKET
+    echo ""
+    read -s -p "Enter S3 region: " S3_REGION
     echo ""
 fi
 
-# location for local backup data
-BACKUP_DIR="/home/forge/backup"
+# locations for local backup data
+FILE_BACKUP_DIR="//files"
+DB_BACKUP_DIR="//mysqldump"
 
 # location of files to back up
 BACKUP_TARGET="/home/forge"
@@ -66,8 +69,14 @@ if [ ! -d /root/restic ]; then
     # create directories we'll need
     mkdir -p /root/restic
     mkdir -p /root/restic/conf
-    mkdir -p $BACKUP_DIR
-    chown -R forge:forge $BACKUP_DIR
+    mkdir -p $FILE_BACKUP_DIR && chown -R forge:forge $FILE_BACKUP_DIR
+    mkdir -p $DB_BACKUP_DIR && chown -R forge:forge $DB_BACKUP_DIR
+
+    # create restic settings file
+    touch /root/restic/conf/restic.conf
+    echo "export FILE_BACKUP_DIR=\"$FILE_BACKUP_DIR\"" >> /root/restic/conf/restic.conf
+    echo "export DB_BACKUP_DIR=\"$DB_BACKUP_DIR\"" >> /root/restic/conf/restic.conf
+    echo "export BACKUP_TARGET=\"$BACKUP_TARGET\"" >> /root/restic/conf/restic.conf
 fi
 
 echo ""
@@ -78,7 +87,7 @@ echo ""
 
 # create or source backup user MySQL password
 if [ ! -f /root/restic/conf/mysql.conf ]; then
-    read -s -p "Enter MySQL password for 'forge': " ROOT_MYSQL_PASSWORD
+    read -s -p "Enter MySQL password for user 'forge': " FORGE_MYSQL_PASSWORD
 
     # generate random 32 character alphanumeric string (upper and lowercase)
     MYSQL_PASSWORD=$(cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 32 | head -n 1)
@@ -87,12 +96,11 @@ if [ ! -f /root/restic/conf/mysql.conf ]; then
     echo "export MYSQL_USER=\"$MYSQL_USER\"" >> /root/restic/conf/mysql.conf
     echo "export MYSQL=\"$MYSQL\"" >> /root/restic/conf/mysql.conf
     echo "export MYSQLDUMP=\"$MYSQLDUMP\"" >> /root/restic/conf/mysql.conf
-    echo "export BACKUP_DIR=\"$BACKUP_DIR\"" >> /root/restic/conf/mysql.conf
 
     # create read-only mysql backup user and store credentials
-    $MYSQL --user="forge" --password="$ROOT_MYSQL_PASSWORD" --execute="CREATE USER '${MYSQL_USER}'@'localhost' IDENTIFIED BY '${MYSQL_PASSWORD}';"
-    $MYSQL --user="forge" --password="$ROOT_MYSQL_PASSWORD" --execute="GRANT SELECT, LOCK TABLES ON *.* TO '${MYSQL_USER}'@'localhost';"
-    $MYSQL --user="forge" --password="$ROOT_MYSQL_PASSWORD" --execute="FLUSH PRIVILEGES;"
+    $MYSQL --user="forge" --password="$FORGE_MYSQL_PASSWORD" --execute="CREATE USER '${MYSQL_USER}'@'localhost' IDENTIFIED BY '${MYSQL_PASSWORD}';"
+    $MYSQL --user="forge" --password="$FORGE_MYSQL_PASSWORD" --execute="GRANT SELECT, SHOW VIEW, PROCESS, LOCK TABLES ON *.* TO '${MYSQL_USER}'@'localhost';"
+    $MYSQL --user="forge" --password="$FORGE_MYSQL_PASSWORD" --execute="FLUSH PRIVILEGES;"
 
     echo "!! created backup user with password: $MYSQL_PASSWORD"
 else
@@ -115,16 +123,16 @@ else
     echo "✓ found backup exclude file"
 fi
 
-if [ ! -f /root/restic/conf/b2.conf ]; then
-    # create B2 settings file
-    touch /root/restic/conf/b2.conf
-
-    echo "export B2_ACCOUNT_ID=\"$B2_ACCOUNT_ID\"" >> /root/restic/conf/b2.conf
-    echo "export B2_ACCOUNT_KEY=\"$B2_ACCOUNT_KEY\"" >> /root/restic/conf/b2.conf
-    echo "export B2_BUCKET=\"$B2_BUCKET\"" >> /root/restic/conf/b2.conf
-    echo "wrote B2 settings"
+if [ ! -f /root/restic/conf/s3.conf ]; then
+    # create S3 settings file
+    touch /root/restic/conf/s3.conf
+    echo "export AWS_ACCESS_KEY_ID=\"$AWS_ACCESS_KEY_ID\"" >> /root/restic/conf/s3.conf
+    echo "export AWS_SECRET_ACCESS_KEY=\"$AWS_SECRET_ACCESS_KEY\"" >> /root/restic/conf/s3.conf
+    echo "export S3_BUCKET=\"$S3_BUCKET\"" >> /root/restic/conf/s3.conf
+    echo "export S3_REGION=\"$S3_REGION\"" >> /root/restic/conf/s3.conf
+    echo "wrote S3 settings"
 else
-    echo "✓ found B2 settings"
+    echo "✓ found S3 settings"
 fi
 
 # create or source restic password
@@ -149,23 +157,25 @@ echo "Running MySQL backup routine..."
 echo "----------------------------------------"
 echo ""
 
+# load variables into session
+source /root/restic/conf/restic.conf
 source /root/restic/conf/mysql.conf
 
 DATESTAMP=$(date +"%F")
 TIMESTAMP=$(date +"%H%M%S")
 
-mkdir -p $BACKUP_DIR/mysql/$DATESTAMP
+mkdir -p $DB_BACKUP_DIR/$DATESTAMP
 
 databases=`$MYSQL --user="$MYSQL_USER" --password="$MYSQL_PASSWORD" --execute="SHOW DATABASES;" | grep -Ev "(Database|information_schema)"`
 
 for db in $databases; do
 if [ $db != "performance_schema" ]&&[ $db != "mysql" ];then
-    FILENAME=$BACKUP_DIR/mysql/$DATESTAMP/$db-$TIMESTAMP.gz
+    FILENAME=$DB_BACKUP_DIR/$DATESTAMP/$db-$TIMESTAMP.gz
 
     echo -e "backing up '$db' → $FILENAME"
 
     # with GZIP
-    $MYSQLDUMP --force --opt --user=$MYSQL_USER -p$MYSQL_PASSWORD --databases $db | gzip > "$FILENAME"
+    $MYSQLDUMP --silent --force --opt --user=$MYSQL_USER -p$MYSQL_PASSWORD --databases $db | gzip > "$FILENAME"
 
     # let the forge user inspect backups
     chown forge:forge $FILENAME
@@ -179,7 +189,7 @@ echo "----------------------------------------"
 echo ""
 
 # prune files more than 7 days old
-find $BACKUP_DIR/mysql/ -mtime +7 -name '*.gz' -execdir rm -- '{}' \;
+find $DB_BACKUP_DIR/ -mtime +7 -name '*.gz' -execdir rm -- '{}' \;
 
 echo "Done."
 EOL
@@ -188,23 +198,24 @@ EOL
 cat >/root/restic/restic-backup.sh <<'EOL'
 #/bin/bash
 
-# load b2.conf environment variables into our session
-. /root/restic/conf/b2.conf
+# load variables into session
+. /root/restic/conf/restic.conf
 . /root/restic/conf/mysql.conf
+. /root/restic/conf/s3.conf
 
 echo ""
 echo "----------------------------------------"
-echo "Running restic backup → $B2_BUCKET..."
+echo "Running restic backup → $S3_BUCKET..."
 echo "----------------------------------------"
 echo ""
 
 # create a mysql backup
 /root/restic/mysql-backup.sh
 
-/usr/bin/restic -r b2:$B2_BUCKET:/ backup /home/forge --exclude-file=/root/restic/conf/excludes.conf --password-file=/root/restic/conf/password.conf
-/usr/bin/restic -r b2:$B2_BUCKET:/ --password-file=/root/restic/conf/password.conf forget --keep-daily 7 --keep-weekly 4 --keep-monthly 12 --keep-yearly 2
-/usr/bin/restic -r b2:$B2_BUCKET:/ --password-file=/root/restic/conf/password.conf prune
-/usr/bin/restic -r b2:$B2_BUCKET:/ --password-file=/root/restic/conf/password.conf check
+/usr/bin/restic -r s3:s3.amazonaws.com/$S3_BUCKET -o s3.region=$S3_REGION backup $BACKUP_TARGET --exclude-file=/root/restic/conf/excludes.conf --password-file=/root/restic/conf/password.conf
+/usr/bin/restic -r s3:s3.amazonaws.com/$S3_BUCKET -o s3.region=$S3_REGION --password-file=/root/restic/conf/password.conf forget --keep-daily 7 --keep-weekly 4 --keep-monthly 12 --keep-yearly 2
+/usr/bin/restic -r s3:s3.amazonaws.com/$S3_BUCKET -o s3.region=$S3_REGION --password-file=/root/restic/conf/password.conf prune
+/usr/bin/restic -r s3:s3.amazonaws.com/$S3_BUCKET -o s3.region=$S3_REGION --password-file=/root/restic/conf/password.conf check
 EOL
 
 # save restic mount script
@@ -216,8 +227,8 @@ echo "Mounting backup at /mnt/restic..."
 echo "----------------------------------------"
 
 mkdir /mnt/restic
-. /root/restic/conf/b2.conf
-restic -r b2:$B2_BUCKET mount /mnt/restic --password-file=/root/restic/conf/password.conf
+. /root/restic/conf/s3.conf
+restic -r s3:s3.amazonaws.com/$S3_BUCKET -o s3.region=$S3_REGION mount /mnt/restic --password-file=/root/restic/conf/password.conf
 echo "Unmount with 'umount /mnt/restic' when you're done!"
 EOL
 
@@ -228,15 +239,15 @@ chmod +x /root/restic/restic-mount.sh
 
 # init backup
 # ```
-# source /root/restic/conf/b2.conf
-# restic -r b2:$B2_BUCKET:/ init
+# source /root/restic/conf/s3.conf
+# restic -r s3:s3.amazonaws.com/$S3_BUCKET init
 # ```
 while true; do
     echo ""
     read -p "Do you want to initialize the restic repo now? [y/n] " yn
     echo ""
     case $yn in
-        [Yy]* ) source /root/restic/conf/b2.conf && restic -r b2:$B2_BUCKET:/ init; break;;
+        [Yy]* ) source /root/restic/conf/s3.conf && restic -r s3:s3.amazonaws.com/$S3_BUCKET -o s3.region=$S3_REGION init; break;;
         [Nn]* ) exit;;
         * ) echo "Please answer yes or no.";;
     esac
@@ -245,6 +256,7 @@ done
 echo ""
 echo "----------------------------------------"
 echo "Looking good!"
+echo ""
 echo "Don't forget to finish setting up restic!"
 echo ""
 echo "- [ ] run a MySQL backup with mysql-backup.sh"
@@ -252,4 +264,3 @@ echo "- [ ] run a filesystem backup with restic-backup.sh"
 echo "- [ ] mount and verify backups with restic-mount.sh"
 echo "- [ ] add scheduler job for restic-backup.sh"
 echo "----------------------------------------"
-
